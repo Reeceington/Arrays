c1bfc86f16ea3ff96c58f6c8ce34b4bc7b759814
Reeceington <115169939+Reeceington@users.noreply.github.com>
2022-10-07 16:10:48 +0100

    Add files via upload

---
+++ learningArrays.js
@@ -0,0 +1,46 @@
+"use strict"
+var space = " "
+var bestVideoGamesList = ["Minecraft", "Ori and the will of the wisps", "It takes two", "Fall guys"];
+    
+console.log(bestVideoGamesList);
+    console.log(bestVideoGamesList[0]+space+space+bestVideoGamesList[2]+space+space+bestVideoGamesList[3]);
+
+var funTimes = ("My favourite Games are:");
+
+    console.log(funTimes[4]);
+    console.log(funTimes[13]);
+    console.log(funTimes+space+bestVideoGamesList[0]+", "+bestVideoGamesList[1]+ ", "+bestVideoGamesList[2]+" and "+bestVideoGamesList[3]+". ");
+
+var numbers  = [1, 2, 3, 4, 5];
+
+   ( numbers[4] );
+    console.log( numbers[numbers.length -1] );
+
+   ( numbers[0] );
+    console.log( numbers[numbers.length -5] );
+    console.log (numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[0]);
+
+    var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    week [1] = "Mardi";
+    week [3] = "Jeudi";
+    console.log (week);
+
+   var numerals = [10, 20, 30, 40, 50];
+   numerals [0] = numerals[0]+10;
+   console.log(numerals[0]);
+
+   numerals [1] = numerals[1]+10;
+   console.log(numerals[1]);
+
+   numerals [2] = numerals[2]+10;
+   console.log(numerals[2]);
+
+   numerals [3] = numerals[3]+10;
+   console.log(numerals[3]);
+
+   numerals [4] = numerals[4]+10;
+   console.log(numerals[4]);
+
+   //Arrays with push and pop
+
+ 
